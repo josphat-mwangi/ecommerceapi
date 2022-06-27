a009a4462295faf2698073972895de7adbced334
josphat mwangi <josphatwanjiruw@gmail.com>
2022-06-27 10:38:47 +0300

    change configuration and incress efficience

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-const express = require('express')
-app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require("dotenv")
-dotenv.config()
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const doenv = require('dotenv');
+const bodyparser = require('body-parser');
+const db = require('./config/database')
+// import Routes
+const authRoute = require('./api/routes/auth');
+const productRoute = require('./api/routes/post');
+const cartegoryRoute = require('./api/routes/cartegory');
+const usersRoute = require('./api/routes/user')
 
-mongoose
-    .connect(process.env.DBURI, { useNewUrlParser: true})
-    .then(()=> console.log("DB Connection Sucessfull"))
-    .catch((err)=>{
-        console.log(err);
-    });
 
+doenv.config();
+
+
+db.connect()
 app.use(cors());
-app.use(express.json());
+app.use(bodyparser.json())
 
+
+
+//route middleware
+app.use('/api/user', authRoute);
+app.use('/api/product', productRoute);
+app.use('/api/cartegory', cartegoryRoute);
+app.use('/api/users', usersRoute);
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server Running'))
