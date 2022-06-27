9e3f3e551b047d4b53ce2fde4368c3b0a3586d12
josphat mwangi <josphatwanjiruw@gmail.com>
2022-06-27 10:31:45 +0300

    add required details to the cart

--- src/api/models/Cart.js
+++ src/api/models/Cart.js
@@ -1,18 +1,50 @@
 const mongoose = require('mongoose');
+const objectID = mongoose.Schema.Types.ObjectId
 
 const CartSchema = mongoose.Schema({
     userId:{
-        type: String,
+        type: objectID,
+        ref: 'User',
         require: true
     },
     products:[
         {
             productId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'PostSchema'
-            }
-        }
-    ]
+                type: objectID,
+                ref: 'Post',
+                required: true
+            },
+            productName: {
+                type: String,
+                required: true,
+            },
+            quantity:{
+                type: String,
+                required: true,
+            },
+            noOfItems:{
+                type: Number,
+                min: 1,
+                default: 1,
+                required: true,
+            },
+            unitPrice:{
+                type: Number,
+                required: true
+            },
+            unitPriceCurrency:{
+                type: String,
+                required: true
+            },
+            
+        },
+        
+    ],
+    totalBill: {
+        type: Number,
+        required: true,
+        default: 0
+    }
 },
     { timestamps: true}
 );
