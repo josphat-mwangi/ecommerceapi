e76fa37aa88a60e6a30770be95a077c57589fc48
josphat mwangi <josphatwanjiruw@gmail.com>
2022-07-15 10:47:32 +0300

    create mpesa middleware for access token and base64 converter

--- src/api/middlewares/authVerify.js
+++ src/api/middlewares/authVerify.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const authBase64 = require("./authBase64");
+const request = require("request");
 
-module.exports = (req, res, next)=>{
+function authVerify (req, res, next){
     const token = req.header('auth-token');
     if(!token) return res.status(401).send('Access Denied');
 
     try{
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
-        next();
+        return next();
 
     }catch(err){
         res.send('invalid token')
@@ -15,3 +18,40 @@ module.exports = (req, res, next)=>{
 
     return next();
 }
+
+function authRole(req, res, next){
+    
+    if(req.user.isAdmin !== true ){
+        res.status(401);
+        return res.send('not allowed')
+    }
+
+    next()
+    
+}
+
+const access_token = (req, res, next) => {
+  let endpoint_url = process.env.authurl;
+  let auth = authBase64(process.env.Consumer_Key, process.env.Consumer_Secret);
+
+  request(
+    {
+      url: endpoint_url,
+      headers: {
+        Authorization: "Basic " + auth,
+      },
+    },
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+      } else {
+        req.access_token = JSON.parse(body).access_token;
+        next();
+      }
+    }
+  );
+};
+
+
+
+module.exports = { authVerify, authRole, access_token }
