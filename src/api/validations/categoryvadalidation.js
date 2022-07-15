b0f1b4584e0d0fb835c12769904c2ce8bbbb50b0
josphat mwangi <josphatwanjiruw@gmail.com>
2022-07-15 10:43:32 +0300

    remove field createby for autogeneration

--- src/api/validations/categoryvadalidation.js
+++ src/api/validations/categoryvadalidation.js
@@ -4,7 +4,7 @@ const joi = require('@hapi/joi');
 const categoryValidation = (data) =>{
     const schema = joi.object({
         name: joi.string().required(),
-        createdby: joi.string().required(),
+      
     });
 
     return schema.validate(data)
